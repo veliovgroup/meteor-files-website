fe9e858a90ef588d9732c5085fd577aa0d400cb0
dr.dimitru <ceo@veliov.com>
2021-01-29 23:34:25 +0200

    🤓 Annotate main.js server module

--- imports/server/main.js
+++ imports/server/main.js
@@ -3,5 +3,12 @@ import { WebAppInternals } from 'meteor/webapp';
 import '/imports/server/files.collection.js';
 import '/imports/server/methods.js';
 import '/imports/server/spiderable.js';
+// service-configuration.js USED TO
+// LOAD 3rd PARTY AUTHENTICATION SETTINGS
+// import '/imports/service-configuration.js';
+
+// image-processing.js USED TO
+// DEFINE IMAGE PROCESSING HELPER
+// import '/imports/image-processing.js';
 
 WebAppInternals.enableSubresourceIntegrity();
