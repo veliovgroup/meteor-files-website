c9706ca4a8c0171a741966e3eebdd42b527070ed
dr.dimitru <ceo@veliov.com>
2021-01-31 01:42:29 +0200

    Codebase styling

--- imports/server/methods.js
+++ imports/server/methods.js
@@ -1,5 +1,5 @@
-import { check }             from 'meteor/check';
-import { Meteor }            from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { Meteor } from 'meteor/meteor';
 import { _app, Collections } from '/imports/lib/core.js';
 
 Meteor.methods({
