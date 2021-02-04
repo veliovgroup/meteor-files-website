833c376b2828fb202c13e1add224f2bc5a3ed83f
dr.dimitru <ceo@veliov.com>
2021-02-04 22:43:37 +0200

    👨‍💻 Select file from index template

--- imports/client/index/index.js
+++ imports/client/index/index.js
@@ -1,5 +1,6 @@
 import { _app, Collections } from '/imports/lib/core.js';
 import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
 
 import '/imports/client/upload/upload-row.js';
 import '/imports/client/listing/listing-row.js';
@@ -7,6 +8,14 @@ import './index.jade';
 
 Template.index.onRendered(function() {
   window.IS_RENDERED = true;
+});
+
+Template.index.events({
+  'click [data-select-file]'(e) {
+    e.preventDefault();
+    $('#userfile').trigger('click');
+    return false;
+  }
 });
 
 Template.index.helpers({
