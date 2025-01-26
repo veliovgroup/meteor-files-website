2c9b96313bf116eb93f208924df3699b9256c55c
dr.dimitru <ceo@veliov.com>
2025-01-26 08:46:31 +0200

    👨‍💻 update spiderable to work with meteor@3

--- imports/server/spiderable.js
+++ imports/server/spiderable.js
@@ -3,11 +3,12 @@ import { WebApp } from 'meteor/webapp';
 import Spiderable from 'meteor/ostrio:spiderable-middleware';
 
 if (Meteor.settings.spiderable?.auth) {
-  WebApp.connectHandlers.use(new Spiderable({
+  const spiderable = new Spiderable({
     debug: Meteor.settings.debug,
     rootURL: process.env.ROOT_URL,
     serviceURL: 'https://render.ostr.io',
     auth: Meteor.settings.spiderable.auth,
     only: [/^\/?$/, /^\/about\/?$/i, /^\/f\/[A-z0-9]{14,20}\/?$/i]
-  }));
+  });
+  WebApp.connectHandlers.use(spiderable.handler);
 }
