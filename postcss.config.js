4d05f1dc4d697ea1d55dda94bc0197971b410a3b
dr.dimitru <ceo@veliov.com>
2025-02-15 00:03:24 +0200

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {
+      overrideBrowserslist: [
+        'last 2 versions',
+        '> 5%',
+        'Firefox ESR',
+        'IE 11'
+      ]
+    },
+  }
+};
